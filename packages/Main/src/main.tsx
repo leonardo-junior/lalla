55c0d53a3bfa35828c73d5728ad4f1954167afc8
Leonardo Nunes <leonardo-junior@users.noreply.github.com>
2023-01-06 02:48:18 -0300

    test change name

--- packages/Main/src/main.tsx
+++ packages/Main/src/main.tsx
@@ -1,5 +1,10 @@
-export const Main = () => {
-  return <div>oi</div>
+type MainProps = {
+  name: string
+  age?: string
+}
+
+export const Main = ({name, age}: MainProps) => {
+  return <div>Mt feliz que deu certo {name} {age}</div>
 }
 
 export default Main
